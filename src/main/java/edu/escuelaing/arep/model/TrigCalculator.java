ac06180873b1737745f4bbea2ced60a3a1f149a2
Andres Rocha <andresrochag15@hotmail.com>
2020-09-04 19:04:24 -0500

    calculadora terminada

--- src/main/java/edu/escuelaing/arep/model/TrigCalculator.java
+++ src/main/java/edu/escuelaing/arep/model/TrigCalculator.java
@@ -2,6 +2,10 @@ package edu.escuelaing.arep.model;
 
 import org.json.JSONObject;
 
+import static spark.Spark.port;
+import static spark.Spark.get;
+
+
 
 /**
  * Hello world!
@@ -9,14 +13,22 @@ import org.json.JSONObject;
  */
 public class TrigCalculator {
 
+
     public static void main(String[] args) {
-        getData("1","sin");
+        port(getPort());
+        get("/calculateTrigonometric",(req,res)->{
+            String operation = req.queryParams("operation");
+            String number = req.queryParams("number");
+            JSONObject jsonObject = new JSONObject();
+            JSONObject respuesta = getData(number,operation);
+            return respuesta;
+        });
     }
 
     public static JSONObject getData(String number, String operator) {
         Calculator calculator = new Calculator();
         JSONObject obj = new JSONObject();
-
+        System.out.println(operator+" "+number);
         if(operator.equals("sin"))
             obj.put("sin",calculator.sin(Double.parseDouble(number)));
         else if(operator.equals("cos"))
@@ -27,5 +39,13 @@ public class TrigCalculator {
             obj.put("operator","Operación Invalida");
 
         return obj;
+    }
+
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 45555;
     }
 }
